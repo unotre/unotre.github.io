a85490eacd1b65f1f1cfc50ff31baf8c897224c9
Vincenzo Rosetta <panino@unotre.com>
2018-01-11 18:43:30 +0100

    Fix per parlare con il form

--- accademia-creativa/js/main.js
+++ accademia-creativa/js/main.js
@@ -36,6 +36,7 @@ function enable_cb() {
 
 function form_submitted(param) {
   console.log("Ciao corradinnnnyyy: " + param);
-  if (param == "form_submitted" )
-  $("#step3").removeClass("disabled");
+  if (param == "form_submitted" ) {
+    $("#step3").removeClass("disabled");
+  }
 }
