00ad6bd61fa72871267d8409c919cc38070158e3
Vincenzo Rosetta <panino@unotre.com>
2018-05-02 17:07:35 +0200

    Aggiornamento Accademia Creativa post evento

--- accademia-creativa/js/main.js
+++ accademia-creativa/js/main.js
@@ -14,6 +14,66 @@ $(document).ready(function(){
     }
   });
 
+  // Slick
+  $('.slideshow').slick({
+    //setting-name: setting-value
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+  });
+
+  // Popup partecipante
+  $('#popup_partecipante').popup({
+    color: 'white',
+    opacity: 1,
+    transition: '0.3s',
+    scrolllock: true,
+    autozindex: true
+  });
+
+  $('.partecipante').each(function (index) {
+    $(this).on('click', (function(){
+      $('#popup_partecipante').popup('hide');
+      // Popola il popup con i tuoi dati
+      $('#popup_partecipante img').attr('src', $(this).data('partecipante-workimage'));
+      $('#popup_partecipante h2').text($(this).data('partecipante-name'));
+      $('#popup_partecipante p#popup_partecipante_bio').text($(this).data('partecipante-bio'));
+      $('#popup_partecipante h3').text($(this).data('partecipante-title'));
+      $('#popup_partecipante p#popup_partecipante_workdesc').text($(this).data('partecipante-workdesc'));
+      // Binda tasti previous / next solo se esiste un sibling adiacente
+      $('#popup_partecipante').removeData('this_partecipante_prev');
+      $('#popup_partecipante').data('this_partecipante_prev', $(this).prev());
+      $('#popup_partecipante').removeData('this_partecipante_next');
+      $('#popup_partecipante').data('this_partecipante_next', $(this).next());
+      $('#popup_partecipante #popup_partecipante_prev').off();
+      $('#popup_partecipante #popup_partecipante_next').off();
+      // Previous
+      if ($(this).prev().hasClass('partecipante')) {
+        $('#popup_partecipante #popup_partecipante_prev').on('click', (function() {
+          $($('#popup_partecipante').data('this_partecipante_prev')).click();
+        }));
+        $('#popup_partecipante #popup_partecipante_prev').show();
+      } else {
+        $('#popup_partecipante #popup_partecipante_prev').hide();
+      };
+      // Next
+      if ($(this).next().hasClass('partecipante')) {
+        $('#popup_partecipante #popup_partecipante_next').on('click', (function() {
+          $($('#popup_partecipante').data('this_partecipante_next')).click();
+        }));
+        $('#popup_partecipante #popup_partecipante_next').show();
+      } else {
+        $('#popup_partecipante #popup_partecipante_next').hide();
+      };
+      // Mostra il popup
+      $('#popup_partecipante').popup('show');
+    }));
+  });
+
+
   checkbox_relatore();
   $("#relatore").click(checkbox_relatore);
 
