bad3cfea8aafcaa1a007eba9fbfee926ad3742e3
Vincenzo Rosetta <panino@unotre.com>
2018-05-08 15:07:42 +0200

    Cambiate quotes

--- 5permille/js/main.js
+++ 5permille/js/main.js
@@ -9,13 +9,13 @@ $(document).ready(function(){
 
   Quotation[0] = "Il cinque <br/>per millantare.";
   Quotation[1] = "Forza <br/>dichiara!";
-  Quotation[2] = "Dichiarazione <br/>d'amore.";
+  Quotation[2] = "A buon <br/>rendere.";
   Quotation[3] = "La scelta <br/>(troppo) giusta.";
   Quotation[4] = "Meno responsabilità, <br/>più emozioni.";
   Quotation[5] = "Fai seguire i fatti <br/>alle dichiarazioni.";
   Quotation[6] = "Dichiarare <br/>con stile.";
   Quotation[7] = "Hai qualcosa <br/>da dichiarare!";
-  Quotation[8] = "Il cinque <br/>per millantare.";
+  Quotation[8] = "A buon <br/>rendere.";
   Quotation[9] = "Meno dichiarazioni, <br/>più azioni.";
   Quotation[10] = "La migliore <br/>destinazione.";
 
